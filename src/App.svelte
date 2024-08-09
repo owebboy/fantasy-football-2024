8a880f5278d9b343e372c29d2cc2516c098bfc17
Oliver Pope <popeoliv@msu.edu>
2024-08-09 00:51:53 -0400

    coloring

--- src/App.svelte
+++ src/App.svelte
@@ -1,100 +1,115 @@
 <script lang="ts">
-  import players, { type Player } from "./players";
+  import players, { type PlayerPosition, type Player } from "./players";
   import { writable } from "svelte/store";
 
   // chunk the players into 12 player chunks, each alternating chunk is reversed
-  const chunkedPlayers: Player[][] = players.reduce(
-    (acc: Player[][], player, idx) => {
+  const chunkedPlayers: Player[][] = players
+    .reduce((acc: Player[][], player, idx) => {
       const chunkIdx = Math.floor(idx / 12);
       if (!acc[chunkIdx]) {
         acc[chunkIdx] = [];
       }
       acc[chunkIdx].push(player);
       return acc;
-    },
-    []
-  );
+    }, [])
+    .map((round, idx) => (idx % 2 === 0 ? round : round.reverse()));
 
   const selectedPlayers = writable<Player[]>([]);
+  const calculatePositionColor = (position: PlayerPosition["position"]) => {
+    switch (position) {
+      case "QB":
+        return "#00509E";
+      case "RB":
+        return "#228B22";
+      case "WR":
+        return "#C60C30";
+      case "TE":
+        return "#FF8C00";
+      case "DST":
+        return "#6A0DAD";
+      case "K":
+        return "#333333";
+      default:
+        return "black";
+    }
+  };
 </script>
 
-<main>
-  <div class="grid">
-    {#each chunkedPlayers as round}
-      <div class="round">
-        {#each round as player}
-          <button
-            class="player"
-            on:click={() => {
-              selectedPlayers.update((players) => {
-                // toggle the player in the selected players array
-                if (players.includes(player)) {
-                  return players.filter((p) => p !== player);
-                }
-                return [...players, player];
-              });
-            }}
-            class:selected={$selectedPlayers.includes(player)}
-            style="background-color: {player.position
-              .split(' ')[0]
-              .toLowerCase()}"
-          >
-            <div class="player-meta">
-              <p class="player-rank">{player.rank}</p>
-              <p class="player-position">{player.position}</p>
-            </div>
-            <p>{player.name}</p>
-            <div>
-              <p>{player.team}</p>
-            </div>
-          </button>
-        {/each}
-      </div>
+<div class="grid">
+  {#each chunkedPlayers as round, idx}
+    {#each round as player}
+      <button
+        class="player"
+        on:click={() => {
+          selectedPlayers.update((players) => {
+            // toggle the player in the selected players array
+            if (players.includes(player)) {
+              return players.filter((p) => p !== player);
+            }
+            return [...players, player];
+          });
+        }}
+        class:selected={$selectedPlayers.includes(player)}
+        style="background-color: {calculatePositionColor(
+          player.position.position
+        )}"
+      >
+        <div class="player-meta">
+          <p class="player-rank">#{player.rank}</p>
+          <p class="player-position">{player.position.position}</p>
+        </div>
+        <p class="player-name">{player.name}</p>
+        <div>
+          <p>{player.team}</p>
+        </div>
+      </button>
     {/each}
-  </div>
-</main>
+  {/each}
+</div>
 
 <style>
-  main {
-    padding: 1rem;
-  }
-
   p {
     margin: 0;
   }
-  .round {
-    display: flex;
-    justify-content: space-between;
-  }
 
-  .round:nth-child(even) {
-    flex-direction: row-reverse;
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(12, 1fr);
+    gap: 1px;
+    place-items: center;
+    width: 1080px;
+    margin: 0 auto;
   }
 
   .player {
+    border-radius: 4px;
+    color: #fff;
+    height: calc(90px);
+    width: calc(90px);
+
     appearance: none;
     padding: 5px;
-    width: 100px;
-    height: 100px;
     display: flex;
     align-items: center;
     flex-direction: column;
     justify-content: space-between;
-    border: 1px solid #aaa;
-    margin: 2px;
+    border: 1px solid #222;
+    margin: 0px;
     cursor: pointer;
     background: transparent;
   }
 
   .selected {
-    background-color: white;
-    color: #222;
-    border-color: white;
+    filter: grayscale(0.5) brightness(0.6);
   }
 
   .player > p {
-    font-size: 1rem;
+    font-size: 0.9rem;
     text-align: center;
+  }
+
+  .player-name {
+    font-weight: 500;
   }
 
   .player-meta {
